1309f98666cf338c9d09942561a3e0be7183ad9d
Awais Amjed <awaisamjed171@gmail.com>
2021-12-22 23:23:36 +0500

    Final Commit

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+function openDrawer() {
+    document.getElementById("side-drawer").style.display = "block";
+}
+
+function closeDrawer() {
+    document.getElementById("side-drawer").style.display = "none";
+}
